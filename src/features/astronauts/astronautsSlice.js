4068d068b3b580c25d532274aceadfd0f50f5f32
Keila Lopez <keilalofra@gmail.com>
2023-03-01 17:13:28 -0800

    lab completed

--- src/features/astronauts/astronautsSlice.js
+++ src/features/astronauts/astronautsSlice.js
@@ -1,14 +1,23 @@
 // Action Creators
-export function fetchAstronauts(astronauts) {
-  return {
-    type: "astronauts/astronautsLoaded",
-    payload: astronauts,
+// src/features/astronauts/astronautsSlice.js
+export function fetchAstronauts() {
+  return function (dispatch) {
+    dispatch({ type: "astronauts/astronautsLoading" });
+    fetch("http://api.open-notify.org/astros.json")
+      .then((response) => response.json())
+      .then((astronauts) =>
+        dispatch({
+          type: "astronauts/astronautsLoaded",
+          payload: astronauts.people,
+        })
+      );
   };
 }
 
 // Reducers
 const initialState = {
   entities: [], //array of astronauts
+  status: "idle", //loading status for fetch
 };
 
 export default function reducer(state = initialState, action) {
@@ -16,9 +25,14 @@ export default function reducer(state = initialState, action) {
     case "astronauts/astronautsLoaded":
       return {
         ...state,
+        status: "idle",
         entities: action.payload,
       };
-
+    case "astronauts/astronautsLoading":
+      return {
+        ...state,
+        status: "loading",
+      };
     default:
       return state;
   }
